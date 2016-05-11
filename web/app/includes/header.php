4d4463afcc578b062b1eab257927421709f888c6
Yuri Delgado <yuricd@bol.com.br>
2016-05-11 17:19:49 -0300

    Alteração: organização de diretórios

---
+++ web/app/includes/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Caronas já</title>
+        <meta name="description" content="Rede social de caronas!">
+        <meta name="keywords" content="Caronas, Rede Social, Transporte, Comunicação">
+        <link rel="stylesheet" href="style.css" />
+        <!-- 
+        <link rel="shortcut icon" href="img/favicon.ico" />
+        <link rel="img_src" href="http://www.yuridelgado.com.br/images/nerd-icon.png" />
+        <base href="http://www.yuridelgado.com.br/"> 
+         -->
+    </head>
+    
+    <body>
+        <header>
+            <div class="wrapper">
+                <div id="logo">
+                    <a href="http://localhost.com">
+                       <img src="resources/logotipo.png" alt ="Caronasjá">             
+                    </a>
+                </div>
+            </div>
+        </header>
+        <div id="menu-principal">
+            <div class="wrapper">
+                <nav id="menu">
+                    <ul>
+                        <li><a href="Home" class="PrincipAtivo">Home</a></li>
+                        <li><a href="Caronas">Caronas</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        <!-- Inicio da página -->
+
+        <div class="content">
