e0f26c61d206694c6436efaee1721563309fb76c
Yuri Delgado <yuricd@bol.com.br>
2016-05-11 17:20:31 -0300

    Criação: index do site

---
+++ web/index.php
@@ -0,0 +1,10 @@
+<?php
+    require_once("config/main.php");
+
+    include_once("app/includes/header.php");
+    echo dirname(__FILE__);
+
+    include_once("app/includes/footer.php");
+?>
+            
+        
