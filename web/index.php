e6e01dd834735d5e4bcb628f0cef1a00380d3759
Yuri Delgado <yuricd@bol.com.br>
2016-05-11 17:53:11 -0300

    Alteração: inclusão de páginas via parâmetro

--- web/index.php
+++ web/index.php
@@ -1,9 +1,18 @@
 <?php
-    require_once("config/main.php");
+    //Arquivo de configuração
+    require_once("protected/config/main.php");
 
+    //Controller 
+    require_once(CONTROLLERS."SiteController.php");
+
+    //Inclusao do cabecalho (topo) do site
     include_once("app/includes/header.php");
-    echo dirname(__FILE__);
+    
+    //Chamada da index do controller
+    require_once(call_user_func(array('SiteController', 'index'), 'home')); 
 
+
+    //Inclusao do rodape do site
     include_once("app/includes/footer.php");
 ?>
             
