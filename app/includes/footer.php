4a31a73f9d1b233d6d392089be60109d0c784b65
igorhitz <igorhitz@gmail.com>
2016-05-16 14:55:58 -0300

    Começo do footer

--- app/includes/footer.php
+++ app/includes/footer.php
@@ -1,4 +1,14 @@
 </div>
-            
+		<div class = "wrapper">
+	    	<footer>
+	    		Desenvolvido por: 
+	    		<ul id = "desenvolvedores">
+	    			<li>Yuri Delgado</li>
+	    			<li>Igor Hitzschky</li>
+	    			<li>Julliany Favinha</li>
+	    			<li>João Vítor Buscatto</li>
+	    		</ul>
+	    	</footer>    
+	    </div>
 	</body>
 </html>
