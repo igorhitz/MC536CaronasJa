6514169eca337f0c31899ea9f0565456a57ffa70
igorhitz <igorhitz@gmail.com>
2016-05-20 13:32:22 -0300

    Criação: Inicio de cadastro de caronas

--- app/pages/caronas.php
+++ app/pages/caronas.php
@@ -1,1 +1,13 @@
-caronas
+<div class = "wrapper">
+	<form action="action_page.php">
+	  <fieldset>
+		<legend>Registrar Carona</legend>
+		Origem:<br>
+		<input type="text" name="Origem"><br>
+		Destino:<br>
+		<input type="text" name="Destino"><br><br>
+		<input type="submit" value="Cadastrar Carona!">
+		
+	  </fieldset>
+	</form>
+</div>
