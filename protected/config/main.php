0886e2095dfd7947c2fddfa2f7cc090e69eded11
igorhitz <igorhitz@gmail.com>
2016-05-20 13:38:43 -0300

    Update main.php

--- protected/config/main.php
+++ protected/config/main.php
@@ -1,9 +1,9 @@
 <?php
 	//Definicao de paths
 	if($_SERVER['SERVER_NAME'] == 'localhost') {
-		define('PATH', 'http://localhost/MC536CaronasJa-v2/app/');
+		define('PATH', 'http://localhost/MC536CaronasJa/app/');
 	} elseif($_SERVER['SERVER_NAME'] == '127.0.0.1') {
-		define('PATH', 'http://127.0.0.1/MC536CaronasJa-v2/app/');
+		define('PATH', 'http://127.0.0.1/MC536CaronasJa/app/');
 	} else {
 		//Endereco online
 	}
