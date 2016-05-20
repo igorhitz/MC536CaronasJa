5dc6410fb9c5237fad1b45b7d7b488648cca2b6b
julianny-favinha <julianny.favinha@gmail.com>
2016-05-20 18:46:14 -0300

    Cadastro de veículo

--- app/includes/header.php
+++ app/includes/header.php
@@ -37,6 +37,7 @@
                     <ul>
                         <li><a href="Home" class="PrincipAtivo">Home</a></li>
                         <li><a href="Caronas">Caronas</a></li>
+						<li><a href="Veiculo">Veiculo</a></li>
                     </ul>
                 </nav>
             </div>
---
+++ app/pages/veiculo.php
@@ -0,0 +1,21 @@
+<div class = "wrapper">
+	<form action="action_page.php">
+	  <fieldset>
+		<legend>Registrar Veículo</legend>
+		Marca:<br>
+		<input type="text" name="Marca"><br>
+		Modelo:<br>
+		<input type="text" name="Modelo"><br>
+		Cor:<br>
+		<input type="text" name="Cor"><br>
+		Quantidade de lugares:<br>
+		<input type="number" name="QtdLugares" min="0" max="4" step="1" value="4"><br>
+		Categoria:<br>
+		<input type="text" name="Categoria"><br>
+		Conforto:<br>
+		<input type="text" name="Conforto"><br><br>
+		<input type="submit" value="Cadastrar Veículo!">
+		
+	  </fieldset>
+	</form>
+</div>
