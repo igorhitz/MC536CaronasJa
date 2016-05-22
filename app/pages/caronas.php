53e2d5a6c9e81a837f9e1cdcfb01ca10ff085c7f
igorhitz <igorhitz@gmail.com>
2016-05-22 12:37:23 -0300

    Criacao: Form de Cadastro de Carona

--- app/pages/caronas.php
+++ app/pages/caronas.php
@@ -1,15 +1,69 @@
 <div class = "wrapper">
-	<form action="action_page.php">
-	  <fieldset>
-		<legend>Registrar Carona</legend>
-		Origem:<br>
-		<input type="text" name="Origem" maxlength="50"><br>
-		Destino:<br>
-		<input type="text" name="Destino" maxlength="50"><br>
-		Descrição:<br>
-		<textarea name="Descricao" rows="5" cols="20"></textarea><br><br>
-		<input type="submit" value="Cadastrar Carona!">
-		
-	  </fieldset>
+	<form id="form1" name="form1" class="wufoo topLabel page1" accept-charset="UTF-8" autocomplete="off" method="post" novalidate
+      action="">
+		<h2>Registrar Carona</h2>
+		<ul>
+			<li id = "field1">
+				<label class = "desc" id = "title1" for = "Field1">Origem</label>
+				<div>
+					<input id = "Field1" name = "Field1" type = "text" class="field text medium" maxlength="50" tabindex = "1">
+				</div>
+			</li>
+			<li id = "field2">
+				<label class = "desc" id = "title2" for = "Field2">Destino</label>
+				<div>
+					<input id = "Field2" name = "Field2" type = "text" class="field text medium" maxlength="50" tabindex = "2"> 
+				</div>
+			</li>
+			<li id = "field3">
+				<label class = "desc" id = "title3" for = "Field3">Data</label>
+				<div>
+					<input id = "Field3" name = "Field3" type = "date" value="2016-05-22" class ="field text" maxlength = "10" tabindex = "3">
+				</div>
+			</li>
+			<li id = "field4">
+				<label class = "desc" id = "title4" for = "Field4">Horário</label>
+				<div>
+					<input id = "Field4" name = "Field4" type = "time" class= "field text small" value size = "10" maxlength = "6" tabindex = "4">
+				</div>
+			</li>
+			<li id = "field5">
+				<label class = "desc" id = "title5" for = "Field5">Valor</label>
+				<span class="symbol"><b>R$</b></span>
+				<div>
+					<input id = "Field5" name = "Field5" type = "text" class= "field text " value size = "11" maxlength = "10" tabindex = "5">
+				</div>
+			</li>
+			<li id = "field6">
+				<label class = "desc" id = "title6" for = "Field6">Passageiros</label>
+				<div>
+					<select id = "Field6" tabindex = "6">
+						<option value ="1">1</option>
+						<option value ="2">2</option>
+						<option value ="3">3</option>
+						<option value ="4">4</option>
+					</select>
+				</div>
+			</li>
+			<li id = "field7">
+				<label class = "desc" id = "title7" for = "Field7">Bagagem</label>
+				<div>
+					<select id = "Field7" tabindex = "7">
+						<option value ="P">Pequena</option>
+						<option value ="M">Média</option>
+						<option value ="G">Grande</option>
+					</select>
+				</div>
+			</li>
+			<li id = "field8">
+				<label class = "desc" id = "title8" for = "Field8">Descrição</label>
+				<div>
+					<textarea id = "Field8" name = "Field8" class = "field textarea medium" spellcheck = "true" rows = "10" cols = "70" tabindex = "8"></textarea>
+				</div>
+			</li>
+			<li>
+				<input type="submit" class = "field bt" value="Publicar Carona!">
+			</li>
+		</ul>
 	</form>
 </div>
