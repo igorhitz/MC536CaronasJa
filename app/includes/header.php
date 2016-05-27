8c10d8083b2cdc8fe27aba6326f1aa77fcb0bcde
Yuri Delgado <yuricd@bol.com.br>
2016-05-27 20:25:32 -0300

    Correção: redefinição de PATHs

--- app/includes/header.php
+++ app/includes/header.php
@@ -35,19 +35,19 @@
             <div class="wrapper">
                 <nav id="menu">
                     <ul>
-                        <li><a href="Home" class="PrincipAtivo"><b>Home</b></a></li>
+                        <li><a href="<?= PATH_HREF ?>Home" class="PrincipAtivo"><b>Home</b></a></li>
                         <li><a><b>Caronas</b></a>
 							<ul>
-								<li><a href="registraCaronas">Cadastrar Carona</a></li>
-								<li><a href="buscaCaronas">Buscar Carona</a></li>
+								<li><a href="<?= PATH_HREF ?>registraCaronas">Cadastrar Carona</a></li>
+								<li><a href="<?= PATH_HREF ?>buscaCaronas">Buscar Carona</a></li>
 							</ul>
 						</li>
-						<li><a href="buscaUsuarios"><b>Usuários</b></a></li>
-						<li><a href="veiculo"><b>Veículo</b></a></li>
-						<li><a href="grupo"><b>Grupos</b></a>
+						<li><a href="<?= PATH_HREF ?>buscaUsuarios"><b>Usuários</b></a></li>
+						<li><a href="<?= PATH_HREF ?>veiculo"><b>Veículo</b></a></li>
+						<li><a href="<?= PATH_HREF ?>grupo"><b>Grupos</b></a>
 							<ul>
-								<li><a href="meusGrupos">Meus Grupos</a></li>
-								<li><a href="buscaGrupos">Buscar Grupos</a></li>
+								<li><a href="<?= PATH_HREF ?>meusGrupos">Meus Grupos</a></li>
+								<li><a href="<?= PATH_HREF ?>buscaGrupos">Buscar Grupos</a></li>
 							</ul>
 						</li>
                     </ul>
