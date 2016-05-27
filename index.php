2c6b47d32ca818a8e39f2928ddb76084d6e745ad
Yuri Delgado <yuricd@bol.com.br>
2016-05-27 20:37:30 -0300

    Alteração: inclusões de arquivos; acesso a actions e exibição de notificações

--- index.php
+++ index.php
@@ -2,17 +2,25 @@
     //Arquivo de configuração
     require_once("protected/config/main.php");
     require_once("protected/classes/SiteHandler.php");
+    require_once("protected/libs/DBOp.php");
+    require_once("protected/classes/Usuario.php");
 
 
     //Inclusao do cabecalho (topo) do site
     include_once("app/includes/header.php");
 
-
     /* Se existirem os parametros passados via get */
     $site = new SiteHandler;
 
     if(isset($_GET['p']) && !empty($_GET['p'])) {
-         require_once($site->getPage($_GET['p']));
+        //exibe notificacao ao usuario se houver
+        $site->getNotification();
+        
+        if($_GET['p'] == 'action') {
+            require_once($site->getAction($_GET['s']));
+        } else {
+           require_once($site->getPage($_GET['p'])); 
+       }
     } else {
         require_once($site->getHome());
     }
