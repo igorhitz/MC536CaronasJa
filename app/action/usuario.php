0c9038645c46de45dfe36c78806c772f16572d3a
Yuri Delgado <yuricd@bol.com.br>
2016-05-27 20:24:57 -0300

    Criação: action de usuário

---
+++ app/action/usuario.php
@@ -0,0 +1,63 @@
+<?php
+	//campos vindos do formulario
+	$campos = array(
+		'nome',
+		'genero',
+		'email',
+		'senha',
+		'foto',
+		'nascimento',
+		'celular'
+		);
+
+	//campos de preenchimento obrigatorio
+	$camposObrigatorios = array(
+		'nome',
+		'genero',
+		'email',
+		'senha',
+		'foto',
+		'nascimento',
+		'celular'
+		);
+
+/** 
+ ** ------------ NAO EDITAR ESTE TRECHO ----------------
+ **/
+	$itensObrigatorios = array();
+	$itens = array();
+
+	//converte itens do array de acordo com o metodo
+	if($_SERVER['REQUEST_METHOD'] == 'POST') {
+		foreach($camposObrigatorios as $item) {
+			$itensObrigatorios[$item] = $_POST[$item];
+		}
+		foreach($campos as $item) {
+			$itens[$item] = $_POST[$item];
+		}
+	} else if($_SERVER['REQUEST_METHOD'] == 'GET') {
+		foreach($camposObrigatorios as $item) {
+			$itensObrigatorios[$item] = $_GET[$item];
+		}
+		foreach($campos as $item) {
+			$itens[$item] = $_GET[$item];
+		}
+	}
+	//verifica se os campos obrigatorios existem e foram preenchidos
+	if(!Usuario::checkAttributes($itensObrigatorios)) {
+		header("Location: ../Home/stat=campos-vazios");
+		exit;
+	}
+
+	$usuario = new Usuario;
+/** --------------------------------------- **/
+	
+	//salva os atributos
+	$usuario->setAttributes($itens['email'], $itens['senha'], $itens['nome'], $itens['genero'], $itens['nascimento'], $itens['celular']);
+	
+	if($usuario->insert()) {
+		header("Location: ../Home/query=".$usuario->encodeQuery());
+		exit;
+	}
+
+?>
