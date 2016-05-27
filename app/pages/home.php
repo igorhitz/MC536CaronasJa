8e597c4df014797b5344353b30903c65f9c825ca
Yuri Delgado <yuricd@bol.com.br>
2016-05-27 20:25:45 -0300

    Alteração: names de inputs

--- app/pages/home.php
+++ app/pages/home.php
@@ -1,15 +1,15 @@
-	<form class="default-form" action="" method="post">
+	<?php (isset($_GET['query'])) ? Usuario::showQuery($_GET['query']) : '' ?>
+	<form class="default-form" action="<?= PATH_HREF ?>action/usuario" method="post">
 		<fieldset>
 			<div class="form-line">
 				<div class="col col6">
 					<label>Nome:</label>
-					<input type="text" name="userName" placeholder="Seu nome completo">
+					<input type="text" name="nome" placeholder="Seu nome completo">
 				</div>
 
 				<div class="col2">
 					<label>Gênero:</label>
-					<select name="userGender">
-						<option>Gênero</option>
+					<select name="genero">
 						<option value="M">Masculino</option>
 						<option value="F">Feminino</option>
 					</select>
@@ -19,21 +19,34 @@
 			<div class="form-line">
 				<div class="col4">
 					<label>E-mail:</label>
-					<input type="text" name="userEmail" placeholder="Seu endereço de email">
+					<input type="text" name="email" placeholder="Seu endereço de email">
 				</div>
 
 				<div  class="col4">
 					<label>Senha:</label>
-					<input type="password" name="userPass" placeholder="Sua senha">
+					<input type="password" name="senha" placeholder="Sua senha">
 				</div>
 			</div>
 
 			<div class="form-line">
 				<div class="col8">
 					<label>Foto de perfil:</label>
-					<input type="text" name="userPhoto" placeholder="URL de foto de perfil">
+					<input type="text" name="foto" placeholder="URL de foto de perfil">
 				</div>
 			</div>
+
+			<div class="form-line">
+				<div class="col4">
+					<label>Data de nascimento:</label>
+					<input type="text" name="nascimento" placeholder="dd/mm/aaaa">
+				</div>
+
+				<div  class="col4">
+					<label>Celular:</label>
+					<input type="text" name="celular" placeholder="Seu número de celular">
+				</div>
+			</div>
+
 
 			<div>
 				<button type="submit" class="btn">Cadastrar</button>
