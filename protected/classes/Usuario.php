15f57837afce9ed5b5e485b9edb5bd51b2cb6f8c
Yuri Delgado <yuricd@bol.com.br>
2016-05-27 20:27:21 -0300

    Criação: métodos que manipulam a exibição da query SQL para o usuário

--- protected/classes/Usuario.php
+++ protected/classes/Usuario.php
@@ -13,8 +13,22 @@
 		
 		public $celular;
 
+		private static $query;
 
-		public function setAttributes($email, $senha, $nome, $genero, $nascimento, $celular) {
+		public function checkAttributes($attributes) {
+			if(is_array($attributes)) {
+				foreach($attributes as $item) {
+					if(!isset($item) || empty($item)) {
+						return false;
+					}
+				}
+			} else {
+				return false;
+			}
+			return true;
+		}
+
+		public function setAttributes($email=null, $senha=null, $nome=null, $genero=null, $nascimento=null, $celular=null) {
 			if(!empty($email)) {
 				$this->email = $email;
 			}
@@ -36,7 +50,31 @@
 		}
 
 		/**
-		 ** Funcao de insert para novo usuario
+		 ** Metodo que retorna ultima query encriptada
+		 ** return @var integer ultimo id inserido
+		**/
+		public static function encodeQuery() {
+			return base64_encode(self::$query);
+		}
+
+		/**
+		 ** Metodo que des(encripta) query
+		 ** return @var integer ultimo id inserido
+		**/
+		public static function decodeQuery($encodedQuery) {
+			return base64_decode($encodedQuery);
+		}
+
+		/**
+		 ** Metodo que des(encripta) query
+		 ** return @var integer ultimo id inserido
+		**/
+		public static function showQuery($query) {
+			return parent::getQueryAlert(self::decodeQuery($query));
+		}
+
+		/**
+		 ** Metodo de insert para novo usuario
 		 ** return @var integer ultimo id inserido
 		**/
 		public function insert() {
@@ -44,14 +82,19 @@
 			if(parent::checkConnection()) {
 				//query para insercao generica
 				$query = "INSERT INTO `usuario`(`email`, `senha`, `nome`, `genero`, `nascimento`, `celular`) VALUES (?,?,?,?,?,?)";
+				self::$query = "INSERT INTO `usuario`(`email`, `senha`, `nome`, `genero`, `nascimento`, `celular`) VALUES ('".$this->email."', '".$this->senha."', '".$this->nome."', '".$this->genero."', '".$this->nascimento."', '".$this->celular."')";
+				
 				//executa a query com prepared statement
 				if($stmt = $this->con->prepare($query)) {
 					$stmt->bind_param('ssssss', $this->email, $this->senha, $this->nome, $this->genero, $this->nascimento, $this->celular);
 					$stmt->execute();
+					return true;
+				} else {
+					return false;
 				}
-				return $this->con->insert_id;
 			} else {
 				parent::getMsg('error', 'Não existe uma conexão com o banco. Inicialize uma antes de realizar essa operação.');
+				return false;
 			}
 		}
 
