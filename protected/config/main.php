aec887aa6e02eb924a9d4152809fe9a40e4fcadc
Yuri Delgado <yuricd@bol.com.br>
2016-05-27 20:27:37 -0300

    Criação: definição de constantes para conexão

--- protected/config/main.php
+++ protected/config/main.php
@@ -2,12 +2,16 @@
 	//Definicao de paths
 	if($_SERVER['SERVER_NAME'] == 'localhost') {
 		define('PATH', 'http://localhost/MC536CaronasJa/app/');
+		define('PATH_HREF', 'http://localhost/MC536CaronasJa/');
 	} elseif($_SERVER['SERVER_NAME'] == '127.0.0.1') {
 		define('PATH', 'http://127.0.0.1/MC536CaronasJa/app/');
+		define('PATH_HREF', 'http://127.0.0.1/MC536CaronasJa/');
 	} else {
 		//Endereco online
 	}
 
-	define('CONTROLLERS', 'protected/controllers/');
-	define('MODELS', 'protected/models/');
+	define('HOST', 'localhost');
+	define('USER', 'root');
+	define('PASS', '');
+	define('DB', 'caronasja');
 ?>
