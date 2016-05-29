110ef3b4b0b816c531bfaa9b67f58af08600f70a
igorhitz <igorhitz@gmail.com>
2016-05-29 20:27:02 -0300

    Fix: img header

--- app/includes/header.php
+++ app/includes/header.php
@@ -47,7 +47,7 @@
         </nav>
             <div class="wrapper">
                 <div id="logo">
-                    <a href="<?= PATH_HREF ?> Home">
+                    <a href="<?= PATH_HREF ?>Home">
                        <img src="<?= PATH.'resources/'?>logotipo.png" alt ="Caronasjá">             
                     </a>
                 </div>
