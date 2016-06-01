978faedf159dec2bf764edd3d17e104ded1338f2
julianny-favinha <julianny.favinha@gmail.com>
2016-05-31 23:17:17 -0300

    Melhorias no design da página Estatísticas

--- app/pages/estatisticas.php
+++ app/pages/estatisticas.php
@@ -11,44 +11,56 @@
 
 	<div class="lefloat">
 		<div class = "card">
-		<h2>Ranking</h2><br>
-		<?php
-			foreach($lista_ranking as $item) {
-		?>
-			<p><?= $item['nome_avaliado'] ?> - <?= number_format($item['media_nota'], 1) ?></p>
-			<br>
-		<?php
-			}
-		?>
+			<h2>Ranking</h2><br>
+			<table>
+			<?php
+				foreach($lista_ranking as $item) {
+			?>
+				<tr>
+					<td><p><b><?= $item['nome_avaliado'] ?></b></p></td>
+					<td><p><?= number_format($item['media_nota'], 1) ?></p></td>
+					<td><img src="<?= PATH.'resources/'?>gold.png" width="25" height="25"></td>
+				</tr>
+			<?php
+				}
+			?>
+			</table>
 		</div>
 	</div>
 
 
 	<div class="rifloat">
 		<div class = "card">
-		<h2>De onde partem mais usuários?</h2>
-		<br>
-		<?php
-			foreach($lista_origem as $item) {
-		?>
-			<p><?= $item['local'] ?> - <?= $item['qtd'] ?> caronas</p>
-			<br>
-		<?php
-			}
-		?>	
+			<h2>De onde partem mais usuários?</h2><br>
+			<table>
+			<?php
+				foreach($lista_origem as $item) {
+			?>
+				<tr>
+					<td><p><b><?= $item['local'] ?></b></p></td>
+					<td><p><?= $item['qtd'] ?> caronas</p></td>
+				</tr>
+
+			<?php
+				}
+			?>
+			</table>
 		</div>
 		<br>
 		
 		<div class = "card">
-		<h2>Para onde chegam mais usuários?</h2>
-		<br>
-		<?php
-			foreach($lista_destino as $item) {
-		?>
-			<p><?= $item['local'] ?> - <?= $item['qtd'] ?> caronas</p>
-			<br>
-		<?php
-			}
-		?>
+		<h2>Para onde chegam mais usuários?</h2><br>
+		<table>
+			<?php
+				foreach($lista_destino as $item) {
+			?>
+				<tr>
+					<td><p><b><?= $item['local'] ?></b></p></td>
+					<td><p><?= $item['qtd'] ?> caronas</p></td>
+				</tr>
+			<?php
+				}
+			?>
+			</table>
 		</div>
 	</div>
